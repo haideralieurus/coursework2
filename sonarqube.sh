ff3f590f2611183222b5df227bd3d00132faf3d7
Ubuntu <ubuntu@ip-172-31-58-102.ec2.internal>
2020-12-14 14:27:46 +0000

    updated sonar

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-docker exec jenkins-container bash 
-cd /var/jenkins_home
-mkdir sonarqube && cd sonarqube
-wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492-linux.zip
-unzip sonar-scanner-cli-3.3.0.1492-linux.zip
-exit
+docker exec jenkins-container mkdir /var/jenkins_home/sonarqube
+docker exec jenkins-container wget -O /var/jenkins_home/sonarqube/sonar-scanner-cli-3.3.0.1492-linux.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492-linux.zip
+docker exec jenkins-container unzip /var/jenkins_home/sonarqube/sonar-scanner-cli-3.3.0.1492-linux.zip 
